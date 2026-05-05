90a073ca3d152f61364c53ec896c5aaac9b73d23
N1xev <alasamouly@gmail.com>
2026-05-05 03:57:32 +0300

    feat(tmux): switch to ANSI colors, remove matugen template
    Use ANSI color names (brightblack, white, cyan, etc.) for pane borders
    and dotbar status bar instead of hex colors from matugen template.
    Set tmux terminal to xterm-256color with RGB feature support so
    programs inside tmux get proper true color passthrough.
    Override dotbar default bg with 'default' for transparent status bar.
    Remove matugen tmux.theme template and if-shell source.

--- modules/home-manager/tmux/config.nix
+++ modules/home-manager/tmux/config.nix
@@ -1,18 +1,14 @@
 { pkgs, ... }:
-
 {
   programs.tmux = {
     enable = true;
     shell = "${pkgs.fish}/bin/fish";
-    terminal = "screen-256color";
+    terminal = "xterm-256color";
     historyLimit = 10000;
-
     plugins = with pkgs.tmuxPlugins; [
-      rose-pine
       sensible
       vim-tmux-navigator
       yank
-      # this is the mf saver
       resurrect
       {
         plugin = continuum;
@@ -23,52 +19,65 @@
           set -g @tmux_pane_tree_install_agent_hooks 1
         '';
       }
-    ];
+      {
+        plugin = pkgs.tmuxPlugins.mkTmuxPlugin {
+          pluginName = "tmux-dotbar";
+          version = "unstable-2025-04-30";
+          src = pkgs.fetchFromGitHub {
+            owner = "vaaleyard";
+            repo = "tmux-dotbar";
+            rev = "main";
+            sha256 = "0k5pcyrd5iisbsg6vmsglsma44gabk8d3rn4bcz23algk5x4m92r";
+          };
+          postInstall = ''
+            ln -s $out/share/tmux-plugins/tmux-dotbar/dotbar.tmux \
+                  $out/share/tmux-plugins/tmux-dotbar/tmux_dotbar.tmux
+          '';
+        };
+        extraConfig = ''
+          set -g @tmux-dotbar-position "bottom"
+          set -g @tmux-dotbar-justify "absolute-centre"
+          set -g @tmux-dotbar-left "true"
+          set -g @tmux-dotbar-right "true"
 
+          # Use terminal colors (no hardcoded bg)
+          set -g @tmux-dotbar-bg "default"
+          set -g @tmux-dotbar-fg "brightblack"
+          set -g @tmux-dotbar-fg-current "white"
+          set -g @tmux-dotbar-fg-session "brightblack"
+          set -g @tmux-dotbar-fg-prefix "cyan"
+
+          set -g @tmux-dotbar-status-right "#[fg=white bg=default] #I#[fg=brightblack ]#{?pane_in_mode,#[bold fg=yellow]  ,#{?window_zoomed_flag,#[bold fg=cyan]#[bg=default]  ,#{?client_prefix,#[bold fg=red]#[bg=default]  ,#[bold fg=blue]#[bg=default]  }}}"
+          set -g @tmux-dotbar-window-status-format "#[bold]●"
+          set -g @tmux-dotbar-window-status-separator " "
+          set -g @tmux-dotbar-maximized-icon " "
+          set -g @tmux-dotbar-show-maximized-icon-for-all-tabs true
+        '';
+      }
+    ];
     extraConfig = ''
       # General Settings
       set -g status-interval 1
       set -g mouse on
       set -g allow-passthrough on
       set-option -sa terminal-overrides ",xterm*:Tc"
-
-      # Pane border
-      setw -g pane-border-status bottom
-      set -g pane-border-format "#[align=centre]#{?pane_active,#[bold],#[fg=black,bg=default]} #{pane_index} • #{?pane_active,#[bold],#[fg=black,bg=default]}#{pane_current_command} "
-      setw -g pane-border-lines single
-
-      # Status Bar Styling
-      set -g status-left ""
-      set -g status-right "#[fg=white bg=default] #S #[fg=brightblack bg=default]•#{?pane_in_mode,#[bold fg=yellow]#[bg=default]  ,#{?window_zoomed_flag,#[bold fg=cyan]#[bg=default]  ,#{?client_prefix,#[bold fg=red]#[bg=default]  ,#[bold fg=blue]#[bg=default]  }}}"
-      set -g status-style bg=default
-      set -g pane-active-border-style "fg=brightblack"
-      set -g pane-border-style "fg=black"
-      set -g window-status-format "#[fg=white bg=default] #I:#W "
-      set -g window-status-current-format "#[fg=red bg=default bold] #I:#W "
-      set -g status-justify "left"
-
+      set -sa terminal-features ",*:RGB"
       # Keybindings
       unbind C-b
       set -g prefix C-Space
       bind C-Space send-prefix
       bind -n M-H previous-window
       bind -n M-L next-window
-
-      set -g base-index 1
-      setw -g pane-base-index 1
-      set-option -g renumber-windows on
-
+      set -g pane-border-status off
+      set -g pane-border-style "fg=brightblack"
+      set -g pane-active-border-style "fg=brightblack"
       set-window-option -g mode-keys vi
       bind-key -T copy-mode-vi v send-keys -X begin-selection
       bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
       bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
-
       bind - split-window -c '#{pane_current_path}'
       bind / split-window -h -c '#{pane_current_path}'
       bind X if-shell '[ #{session_windows} -gt 1 ]' 'kill-window' 'new-window \; kill-window -t #{window_id}:-1'
-
-      # External Theme
-      if-shell "test -f ~/.cache/matugen/tmux.theme" "source-file ~/.cache/matugen/tmux.theme"
     '';
   };
 }
